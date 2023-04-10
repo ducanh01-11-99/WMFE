2f72c0c8a59aeeb19b88b6a809efebd518d54ccc
ducanh20176919@gmail.com <ducanh20176919@gmail.com>
2023-04-10 13:26:01 +0700

    Anh them giao dien xem map

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Button } from 'antd';
+import GoogleMapReact from 'google-map-react';
 import { LoginStyled, LoginWrapper } from './stylesLogin';
-import Banner from '../HomePage/component/Banner';
+
+// eslint-disable-next-line react/prop-types
+const AnyReactComponent = ({ text }) => <Button>{text} 60%</Button>;
 
 const Login = () => {
-  const { t } = useTranslation();
+  console.log(1234);
   return (
     <LoginStyled>
       <LoginWrapper>
-        <div>Loginhere</div>
+        <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: 'AIzaSyB1fbo7x3EFQrDkKHw70pLIRpKwZXELbuU',
+            }}
+            defaultCenter={{ lat: 21, lng: 105 }}
+            defaultZoom={11}
+          >
+            <AnyReactComponent lat={21.0278} lng={105.8342} text="TR 1" />
+
+            <AnyReactComponent lat={21.027} lng={105.8347} text="TR 2" />
+
+            <AnyReactComponent lat={21.027} lng={105.835} text="TR 3" />
+          </GoogleMapReact>
+        </div>
       </LoginWrapper>
     </LoginStyled>
   );
